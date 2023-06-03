1abbcdfc639a30077a8a13dbf74448c09687c0d4
montezdesousa <montezdesousa@gmail.com>
2023-06-03 17:05:12 +0100

    add maintenance warning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -97,11 +97,7 @@ const App = () => {
             <div className="results">
               <div className="results__title">
                 {img && res.length === 0 ? (
-                  <div className="spinner">
-                    <div className="bounce1"></div>
-                    <div className="bounce2"></div>
-                    <div className="bounce3"></div>
-                  </div>
+                  <div className="spinner">Esta funcionalidade está em manutenção e será reposta em breve. Obrigado!</div>
                 ) : (
                   <></>
                 )}
